86b6e8f39e82782d3f7bedfc05371ab0f5b335cc
burias <nickxaloja@gmail.com>
2019-03-27 21:53:14 -0400

    Added unit tests for Teacher/Student selection and item selection.

--- src/echeckout/CheckoutTester.java
+++ src/echeckout/CheckoutTester.java
@@ -14,49 +14,49 @@ public class CheckoutTester {
     //Test User selection
     public void userSelectionTester() {
         CheckoutClass myCheckoutClassTester = new CheckoutClass();
-        
+
         //Attempt to select Teacher
         myCheckoutClassTester.setuserSelection("Teacher");
-        if(!myCheckoutClassTester.getuserSelection().equals("Teacher")){
+        if (!myCheckoutClassTester.getuserSelection().equals("Teacher")) {
             System.out.println("Teacher selection test FAILED.");
-        }
-        else
+        } else {
             System.out.println("Teacher selection test PASSED.");
-        
+        }
+
         //Attempt to select Student
         myCheckoutClassTester.setuserSelection("Student");
-        if(!myCheckoutClassTester.getuserSelection().equals("Student")){
+        if (!myCheckoutClassTester.getuserSelection().equals("Student")) {
             System.out.println("Student selection test FAILED.");
-        }
-        else
+        } else {
             System.out.println("Student selection test PASSED.");
+        }
     }
-    
-    public void itemSelectorTester(){
+
+    public void itemSelectorTester() {
         CheckoutClass myCheckoutClassTester = new CheckoutClass();
-        
+
         //Attempt to select Laptop
         myCheckoutClassTester.setitemSelection("Laptop");
-        if(!myCheckoutClassTester.getitemSelection().equals("Laptop")){
+        if (!myCheckoutClassTester.getitemSelection().equals("Laptop")) {
             System.out.println("Laptop selection test FAILED.");
-        }
-        else
+        } else {
             System.out.println("Laptop selection test PASSED.");
-        
+        }
+
         //Attempt to select Tablet
         myCheckoutClassTester.setitemSelection("Tablet");
-        if(!myCheckoutClassTester.getitemSelection().equals("Tablet")){
+        if (!myCheckoutClassTester.getitemSelection().equals("Tablet")) {
             System.out.println("Tablet selection test FAILED.");
-        }
-        else
+        } else {
             System.out.println("Tablet selection test PASSED.");
-        
+        }
+
         //Attempt to select Calculator
         myCheckoutClassTester.setitemSelection("Calculator");
-        if(!myCheckoutClassTester.getitemSelection().equals("Calculator")){
+        if (!myCheckoutClassTester.getitemSelection().equals("Calculator")) {
             System.out.println("Calculator selection test FAILED.");
-        }
-        else
+        } else {
             System.out.println("Calculator selection test PASSED.");
+        }
     }
 }
