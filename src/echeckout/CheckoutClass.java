c9c8663a5d05cef22cd81e3a3442dfe37378569a
burias <burias@IST1026-26.floridapoly.org>
2019-04-02 15:05:54 -0400

    Improved class instance verifier

--- src/echeckout/CheckoutClass.java
+++ src/echeckout/CheckoutClass.java
@@ -1,29 +1,48 @@
-
 package echeckout;
 
- class CheckoutClass {
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *
+ * @author Tanner Graham
+ */
+public class CheckoutClass {
+
     private String userSelection;
     private String itemSelection;
+    //Counter to show how many instances of the class have been created
+    private final AtomicInteger counter = new AtomicInteger();
+
+    public CheckoutClass() {
+        counter.incrementAndGet();
+    }
     
-    public void setuserSelection(String userSelection){
+    
+    public void setuserSelection(String userSelection) {
         this.userSelection = userSelection;
     }
-    public String getuserSelection(){
+
+    public String getuserSelection() {
         return userSelection;
     }
-    public void setitemSelection(String itemSelection){
+
+    public void setitemSelection(String itemSelection) {
         this.itemSelection = itemSelection;
     }
-    public String getitemSelection(){
+
+    public String getitemSelection() {
         return itemSelection;
     }
-    public void teacherSelection(String userSelection){
-        if("Teacher".equals(this.userSelection)){
-        String Teacher = this.userSelection;
-        
+
+    public void teacherSelection(String userSelection) {
+        if ("Teacher".equals(this.userSelection)) {
+            String Teacher = this.userSelection;
+
         }
     }
-    
-    
-}
 
+    public int getInstanceCount() {
+        return counter.get();
+    }
+
+}
